70f411a1924d78fb3b234827f6203ea1251fa0c7
Edmund Wippler <ed.wippler@gmail.com>
2019-02-08 15:34:05 -0600

    Updated score route to return team alias and updated readme to include score route definition.

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 The goal of this exercise is to create an end-to-end Proof-of-Concept for an NFL data API, which will aggregate season data based on various parameters and return it as JSON to the caller.
 
+## GET /score/averageSinceBye/{teamAbbreviation}[?period={optionalPeriod}]
+If no period is specified, the average returned will be based on the total score.  
+Response 200: JSON describing team and average score.tExample: <br>
+`{"team": "NO","average_score": "5"}` <br>
+Response 404: No data found for team/period combination
+
 ## Run Local Instructions:
 #### Prerequisites
 Node installed on local machine  
-PostreSQL installed on local machine
+PostreSQL installed on local machine (A PostgreSQL client such as Postico may be useful as well)
 #### To install on a local PC running macOS
 Clone repo  
 Install dependencies using `npm install`  
--- server/routes/score.route.js
+++ server/routes/score.route.js
@@ -32,14 +32,15 @@ router.get('/averageSinceBye/:team', (req, res) => {
       // Use the resut of the bye week to query average score
       let byeWeek = result.rows[0].bye_week
       let selectedPeriod = self.determinePeriod(inputPeriod)
-      let scoreQueryText = `SELECT AVG(${selectedPeriod}) AS average FROM team_score WHERE team_abbr = $1 AND week > $2`
+      let scoreQueryText = `SELECT team_abbr AS team, AVG(${selectedPeriod}) AS average_score FROM team_score` +
+      ' WHERE team_abbr = $1 AND week > $2 GROUP BY team_abbr'
       pool.query(scoreQueryText, [teamAlias, byeWeek])
         .then((result) => {
-          res.send(result.rows)
+          res.status(200).send(result.rows)
         })
         .catch((error) => {
           console.log(`Error requesting score data: ${error}`)
-          res.send(`Unable to find data for ${teamAlias} in period ${inputPeriod}.\r\nPlease verify team and period provided.`)
+          res.status(404).send(`Unable to find data for ${teamAlias} in period ${inputPeriod}.\r\nPlease verify team and period provided.`)
         })
     })
     .catch((error) => {
