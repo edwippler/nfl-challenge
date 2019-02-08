b668d6d012e8ab954e96665d6f395f1ef5275583
Edmund Wippler <ed.wippler@gmail.com>
2019-02-08 15:05:54 -0600

    Completed score route to accept required team input and optional period input to return average score

--- server/routes/score.route.js
+++ server/routes/score.route.js
@@ -4,29 +4,46 @@
 const router = require('express').Router()
 const pool = require('../modules/pool')
 const queries = require('../constants/queries')
+const self = this
 
-// Score Section
+/**
+ * Helper function to define the period text to be queried
+ * Uses point_total if no period defined
+ */
+self.determinePeriod = (selectedPeriod) => {
+  let period = 'point_total'
+  if (selectedPeriod === 'OT') {
+    period = 'point_ot'
+  } else if (selectedPeriod) {
+    period = `point_q${selectedPeriod}`
+  }
+  return period
+}
+
+/**
+ * Main Score funtions section to return average score
+ */
 router.get('/averageSinceBye/:team', (req, res) => {
   let teamAlias = req.params.team
-  let periodSpecified = req.query.period
-  // TODO: make period and bye week dynamic
-  let scoreQueryText = 'SELECT AVG(point_total) AS average FROM team_score WHERE team_abbr = $1 AND week > $2;'
-
+  let inputPeriod = req.query.period
+  // Query for bye week first
   pool.query(queries.getTeamByeWeekQuery, [teamAlias])
     .then((result) => {
-      // console.log(`Period desired: ${periodSpecified}`)
+      // Use the resut of the bye week to query average score
       let byeWeek = result.rows[0].bye_week
+      let selectedPeriod = self.determinePeriod(inputPeriod)
+      let scoreQueryText = `SELECT AVG(${selectedPeriod}) AS average FROM team_score WHERE team_abbr = $1 AND week > $2`
       pool.query(scoreQueryText, [teamAlias, byeWeek])
         .then((result) => {
           res.send(result.rows)
         })
         .catch((error) => {
           console.log(`Error requesting score data: ${error}`)
-          res.sendStatus(500).send(`Unable to get average score for ${teamAlias}.`)
+          res.send(`Unable to find data for ${teamAlias} in period ${inputPeriod}.\r\nPlease verify team and period provided.`)
         })
     })
     .catch((error) => {
-      console.log(`Error in score.route when provided Team: ${teamAlias} and Period: ${periodSpecified}: ${error}`)
+      console.log(`Error in score.route when provided Team: ${teamAlias} and Period: ${inputPeriod}: ${error}`)
       res.sendStatus(500)
     })
 })
